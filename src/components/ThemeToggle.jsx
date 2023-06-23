d09ca02ad59c5d11f58199e6e0c9b04f5b530bb2
Giovanny <giovannyrm.8@gmail.com>
2023-06-23 18:39:51 -0500

    some changes

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 
+import sunIcon from "../assets/sun-icon.svg";
+
 export default function ThemeToggle() {
   const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
 
   const toggleThemeMode = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    setTheme(theme === "dark" ? "light" : "dark");
     document.documentElement.classList.add("animate-smoothBg");
     setInterval(() => {
       document.documentElement.classList.remove("animate-smoothBg");
@@ -12,10 +14,14 @@ export default function ThemeToggle() {
   };
 
   useEffect(() => {
-    if (theme === "light") {
-      document.documentElement.classList.add("light");
+    if (
+      theme == "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove("light");
+      document.documentElement.classList.remove("dark");
     }
 
     localStorage.setItem("theme", theme);
@@ -24,9 +30,9 @@ export default function ThemeToggle() {
   return (
     <button onClick={toggleThemeMode}>
       {theme === "light" ? (
-        <i className="fa-solid fa-moon"></i>
+        <i className="fa-solid fa-moon bg-black text-[#1c93dc] p-1 rounded w-7 "></i>
       ) : (
-        <i className="fa-solid fa-sun"></i>
+        <i className="fa-solid fa-sun bg-[#1c93dc] dark:text-black p-1 rounded w-7"></i>
       )}
     </button>
   );
